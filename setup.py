dd6e7f105785f1c4c470ac2718556a835e6039c4
Rio Adino <riojosepha@gmail.com>
2021-04-26 23:22:40 -0400

    tgchizu-1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="tgchizu",
-    version="1.3.1",
+    version="1.3.2",
     author="reaitten",
     author_email="riojosepha@gmail.com",
     description="A Telegram Bot written in Python language to mirror files on the internet to Google Drive",
